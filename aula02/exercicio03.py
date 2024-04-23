278813a4ec6565005fadf4067071b187f3d55c70
lamorasjr <lamorasjr@gmail.com>
2024-04-23 23:33:41 +0200

    adicionado solução do exercicio03

---
+++ aula02/exercicio03.py
@@ -0,0 +1,7 @@
+#### Exercicio de Inteiros (`int`)
+
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+valor1 = int(input("Digite um valor: "))
+valor2 = int(input("Digite outro valor: "))
+multiplicao = valor1 * valor2
+print(f"A multiplicação destes valores é {multiplicao}.")
