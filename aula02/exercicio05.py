472ea76c58adaf6af770218cd5e646ae3405fc15
lamorasjr <lamorasjr@gmail.com>
2024-04-23 23:41:55 +0200

    adicionado solucao exercicio05

--- aula02/exercicio05.py
+++ aula02/exercicio05.py
@@ -1,3 +1,6 @@
 #### Exercicio de Inteiros (`int`)
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+num = int(input("Digite um valor: "))
+quadrado_num = num ** 2
+print(f"O quadrado deste número é {quadrado_num}.")
