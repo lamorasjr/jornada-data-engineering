c8d82ad584db37957309907df66ec3f210d87564
lamorasjr <lamorasjr@gmail.com>
2024-04-23 23:31:07 +0200

    adicionado solução do exercico02

---
+++ aula02/exercicio02.py
@@ -0,0 +1,7 @@
+#### Exercicio de Inteiros (`int`)
+
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+DENOMINADOR = 5
+valor1 = int(input("Digite um valor: "))
+resto_divisao = valor1 % DENOMINADOR
+print(f"Divido por 5, o resto da divisão deste valor é {resto_divisao}.")
