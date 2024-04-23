6837d716c71beaee193320354a9ff8e070e43a8d
lamorasjr <lamorasjr@gmail.com>
2024-04-23 23:30:11 +0200

    adicionado solução do exercicio01

---
+++ aula02/exercicio01.py
@@ -0,0 +1,7 @@
+#### Exercicio de Inteiros (`int`)
+
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+valor1 = int(input("Digite um valor: "))
+valor2 = int(input("Digite outro valor: "))
+soma = valor1 + valor2
+print(f"A soma dos valores é {soma}.")
