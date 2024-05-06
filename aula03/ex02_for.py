e2e5e5b5e6a952395f20230ff2ae7e9dd2376d9f
lamorasjr <lamorasjr@gmail.com>
2024-05-06 23:11:13 +0200

    adicionado exemplo 02 aula 03 - for loop

---
+++ aula03/ex02_for.py
@@ -0,0 +1,12 @@
+# utilizado quanto eu tenho uma condicao conhecida de parada
+# comeca do primeiro e para antes do ultimo valor --> caracterisca to range()
+
+# for i in range(1,5):
+#     print(i)
+
+# o for itera sobre "qualquer coisa"
+
+lista_de_alunos = ["rafael", "fabio", "luciano"]
+
+for aluno in lista_de_alunos:
+    print(aluno)
