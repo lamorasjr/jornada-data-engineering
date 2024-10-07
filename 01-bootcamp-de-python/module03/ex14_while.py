a8bef3e7fefc091552e75474171de6123e0d687f
lamorasjr <lamorasjr@gmail.com>
2024-10-07 23:15:45 +0200

    updated bootcamp-python-module3 ex14_for.py

--- 01-bootcamp-de-python/module03/ex14_while.py
+++ 01-bootcamp-de-python/module03/ex14_while.py
@@ -1,2 +1,16 @@
 ### Exercício 14. Tentativas de Conexão
 # Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
+
+maximum_attemps = 5
+attempt_number = 1
+
+while attempt_number <= maximum_attemps:
+    print(f'Connection attempt(s): {attempt_number} of {maximum_attemps}')
+
+    if False:
+        print('Connection sucessfully established.')
+        break
+    
+    attempt_number += 1
+else:
+    print('Failed to connect after multiple attempts.')
