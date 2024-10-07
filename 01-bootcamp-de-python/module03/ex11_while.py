7cbe5206b75388ee56dac65d8e3750b8eafc45e3
lamorasjr <lamorasjr@gmail.com>
2024-10-07 22:56:40 +0200

    updated bootcamp-python-module3 ex11_for.py

--- 01-bootcamp-de-python/module03/ex11_while.py
+++ 01-bootcamp-de-python/module03/ex11_while.py
@@ -1,2 +1,11 @@
 ## Exercício 11. Leitura de Dados até Flag
 # Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
+
+
+dados = []
+entrada = ""
+
+while entrada.lower() != "sair":
+    entrada = input("Digite um valor (ou 'sair' para terminar): ")
+    if entrada.lower() != "sair":
+        pass
