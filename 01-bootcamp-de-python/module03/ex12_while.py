bcb680374800923590e5b8b1387ee53b19964982
lamorasjr <lamorasjr@gmail.com>
2024-10-07 23:08:20 +0200

    updated bootcamp-python-module3 ex12_for.py

--- 01-bootcamp-de-python/module03/ex12_while.py
+++ 01-bootcamp-de-python/module03/ex12_while.py
@@ -1,2 +1,12 @@
 ### Exercício 12. Validação de Entrada
 # Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
+
+
+number = int(input("Insert a number between 1 and 10: "))
+
+while number < 1 or number > 10:
+    print("Number out of the interval!")
+
+    number = int(input("Please, insert a number between 1 and 10: "))
+
+print("Valid number!")
