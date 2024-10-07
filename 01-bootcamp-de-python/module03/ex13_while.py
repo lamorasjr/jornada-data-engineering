25efbfaa5286e088e7b3378309c8c92cf0e0a677
lamorasjr <lamorasjr@gmail.com>
2024-10-07 23:12:02 +0200

    updated bootcamp-python-module3 ex13_for.py

--- 01-bootcamp-de-python/module03/ex13_while.py
+++ 01-bootcamp-de-python/module03/ex13_while.py
@@ -1,3 +1,13 @@
 ### Exercício 13. Consumo de API Simulado
 # Simular o consumo de uma API paginada, 
 # onde cada "página" de dados é processada em loop até que não haja mais páginas.
+
+current_page = 1
+total_pages = 5  # Simulação, na prática, isso viria da API
+
+while current_page <= total_pages:
+    print(f'Number of pages processed: {current_page} of {total_pages}')
+
+    current_page += 1
+
+print('All pages have been processed.')
