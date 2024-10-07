71835dc9cfc4a8ee60bd95fbdf45a287deceb8eb
lamorasjr <lamorasjr@gmail.com>
2024-10-07 22:20:01 +0200

    created bootcamp-python module03 ex08_for.py”

--- 01-bootcamp-de-python/module03/ex08_for.py
+++ 01-bootcamp-de-python/module03/ex08_for.py
@@ -1,3 +1,17 @@
 ### Exercício 8. Filtragem de Dados Faltantes
 # Objetivo:** Dada uma lista de dicionários representando dados de usuários, 
 # filtrar aqueles que têm um campo específico faltando
+
+users = [
+    {"nome": "Alice", "email": "alice@example.com"},
+    {"nome": "Bob", "email": ""},
+    {"nome": "Carol", "email": "carol@example.com"}
+]
+
+valid_users = []
+
+for user in users:
+    if user['email']:
+        valid_users.append(user)
+
+print(valid_users)
