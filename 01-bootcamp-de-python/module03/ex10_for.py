ea8749f405bdf330c7b7eca890018a5ab9d2346b
lamorasjr <lamorasjr@gmail.com>
2024-10-07 22:53:13 +0200

    updated bootcamp-python-module3 ex10_for.py

--- 01-bootcamp-de-python/module03/ex10_for.py
+++ 01-bootcamp-de-python/module03/ex10_for.py
@@ -1,2 +1,22 @@
 ### Exercício 10. Agregação de Dados por Categoria
 # Objetivo:** Dado um conjunto de registros de vendas, calcular o total de vendas por categoria.
+
+vendas = [
+    {"categoria": "eletrônicos", "valor": 1200},
+    {"categoria": "livros", "valor": 200},
+    {"categoria": "eletrônicos", "valor": 800}
+]
+
+vendas_groupby = {}
+
+for venda in vendas:
+    categoria = venda["categoria"]
+    valor = venda["valor"]
+    
+    if categoria in vendas_groupby:
+        vendas_groupby[categoria] += valor
+    else:
+        vendas_groupby[categoria] = valor
+
+
+print(vendas_groupby)
