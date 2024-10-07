28a22eef57680584f6afd124d3a1f10cd535c81c
lamorasjr <lamorasjr@gmail.com>
2024-10-07 22:25:14 +0200

    updated bootcamp python module 3 ex08_for.py

--- 01-bootcamp-de-python/module03/ex09_for.py
+++ 01-bootcamp-de-python/module03/ex09_for.py
@@ -1,2 +1,14 @@
 ### Exercício 9. Extração de Subconjuntos de Dados
 # Objetivo:** Dada uma lista de números, extrair apenas aqueles que são pares.
+
+numbers = range(1,11)
+
+even_numbers = []
+
+# for number in numbers:
+#     if number % 2 == 0:
+#         even_numbers.append(number)
+
+even_numbers = [ number for number in numbers if number % 2 == 0 ]
+
+print(even_numbers)
