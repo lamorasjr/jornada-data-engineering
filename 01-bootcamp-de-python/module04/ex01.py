1e8a00e80bd9af24a2d14898125af4b46cbac804
lamorasjr <lamorasjr@gmail.com>
2024-10-08 20:31:40 +0200

    created file: bootcamp python module4 ex_01

--- 01-bootcamp-de-python/module04/ex01.py
+++
@@ -1,8 +0,0 @@
-# TypeHing
-
-idade: int = 30
-altura: float = 1.75
-nome: str = "Alice"
-id_estudante: bool = True
-
-print(idade)
--- 01-bootcamp-de-python/module04/ex02.py
+++
@@ -1,7 +0,0 @@
-idade = int(input("Digite a sua idade: "))
-
-if isinstance(idade, int):
-    ... 
-
-
-print(type(idade))
