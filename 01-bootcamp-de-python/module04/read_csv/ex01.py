62b6e56942418ea42af66c391ba07ec8863a0bf3
lamorasjr <lamorasjr@gmail.com>
2024-10-08 21:43:09 +0200

    read csv exercise module4 bootcamp python

---
+++ 01-bootcamp-de-python/module04/read_csv/ex01.py
@@ -0,0 +1,21 @@
+import csv
+
+# Path to the csv file
+file_path = '/Users/lamorasjr/Downloads/jornada-data-engineering/01-bootcamp-de-python/module04/data/exemplo.csv'
+
+# Starts an empty list to store the data
+file_data = []
+
+# Use the context manager 'with' to open the file
+with open(file_path, mode='r', encoding='utf-8') as file:
+    # Create an object to the csv reader
+    csv_reader = csv.DictReader(file)
+
+    # Iterate on top on the rows from the csv file
+    for row in csv_reader:
+        # Add each row (dict type) to a list of data
+        file_data.append(row)
+
+# Display the output results from the csv file
+for i in file_data:
+    print(i)
