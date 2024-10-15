76b9e318e2af53a51ac5fa7fa0910f5fe4958d1c
lamorasjr <lamorasjr@gmail.com>
2024-10-15 20:46:23 +0200

    added solution py m07 e02

--- 01-bootcamp-de-python/mod07/ex02.py
+++ 01-bootcamp-de-python/mod07/ex02.py
@@ -1,1 +1,20 @@
 #02 - Filtrar Dados Acima de um Limite
+
+from typing import List
+
+
+def filter_greater_than(values_list: List[float], lower_limit: float) -> List[float]:
+    result_list = []
+
+    for v in values_list:
+        if v > lower_limit:
+            result_list.append(v)
+    
+    return result_list
+
+
+ex_list = [1, 15, 16, 30, 50]
+
+ex_result = filter_greater_than(ex_list, 18)
+print(ex_result)
+
