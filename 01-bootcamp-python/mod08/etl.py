307f15288844227b50d10dbcf8df94d896e7b9f4
lamorasjr <lamorasjr@gmail.com>
2024-10-20 22:49:45 +0200

    build: etl to read json file and load to csv and or parquet

---
+++ 01-bootcamp-python/mod08/etl.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import os
+import glob
+
+# Function that extracts, read and consolidates json file
+def extract_data(folder: str) -> pd.DataFrame:
+    json_files = glob.glob(os.path.join(folder, '*.json'))
+    df_list = [ pd.read_json(file) for file in json_files ]
+    df_total = pd.concat(df_list, ignore_index=True)
+    return df_total
+
+# Function to transform
+def transform_data(df: pd.DataFrame) -> pd.DataFrame:
+    df['Total'] = df['Quantidade'] * df['Venda']
+    return df
+
+# Function to load in CSV or Parquet
+def load_data(df: pd.DataFrame, output_formats: list):
+    for format in output_formats:
+        if format == 'csv':
+            df.to_csv('01-bootcamp-python/mod08/sample_data.csv')
+        if format == 'parquet':
+            df.to_parquet('01-bootcamp-python/mod08/sample_data.parquet', index=False)
+
+
+def pipeline_calculate_sales_kpi_consolidated(folder: str, output_formats: list):
+        df_raw = extract_data(folder)
+        df_transformed = transform_data(df_raw)
+        load_data(df_transformed, output_formats)
+        print("ETL successfully completed.")
+
+
+# if __name__ == '__main__':
+#     files_folder1: str = 'data'
+#     output_formats1: list = ['csv', 'parquet']
+#     pipeline_calculate_sales_kpi_consolidated(files_folder1, output_formats1)
---
+++ 01-bootcamp-python/mod08/pipeline.py
@@ -0,0 +1,6 @@
+from etl import pipeline_calculate_sales_kpi_consolidated
+
+files_folder = '01-bootcamp-python/mod08/data'
+data_output_format = ['csv', 'parquet']
+
+pipeline_calculate_sales_kpi_consolidated(files_folder, data_output_format)
