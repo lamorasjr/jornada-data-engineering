ff1314bd1d8e7adf880091fe0d1ed61d9240e0b5
lamorasjr <lamorasjr@gmail.com>
2024-10-26 18:38:49 +0200

    mod01 about classes

---
+++ 01-bootcamp-python/module11-15/01-basic/functional.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def load_csv_and_filter(csv_file, geo_state):
+    # Load a CSV file into a DataFrame
+    df = pd.read_csv(csv_file)
+
+    # Check and remove empty rows 
+    df = df.dropna()
+
+    # Fiter rows by each geo-state
+    df_filtered = df[df['estado'] == geo_state]
+
+    return df_filtered
+
+
+# Use case example
+csv_file = '01-bootcamp-python/module11-15/01-basic/example.csv'  # change this path accordingly with you CSV file
+filtered_state = 'SP'
+df_filtered = load_csv_and_filter(csv_file, filtered_state)
+
+print(df_filtered)
+
+
